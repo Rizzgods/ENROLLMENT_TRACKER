648d1e9dde598764ae184061ba793dc61a31f4ad
yuwan <jcandaya0616@gmail.com>
2025-02-12 16:05:34 +0800

    banner php

--- pre_enroll/header.php
+++ pre_enroll/header.php
@@ -15,18 +15,4 @@
         </div>
     </header>
 
-    <div class="w-full">
-        <div class="hero bg-cover bg-center h-96 md:h-[600px] lg:h-[800px]" style="background-image: url('assets/bestlink.jpg');">
-            <div class="flex items-center justify-center h-full bg-black bg-opacity-50 text-center px-4">
-                <div class="text-center whitespace-normal px-4 md:px-8">
-                    <h1 class="text-4xl md:text-6xl font-bold text-white">Welcome to Bestlink College of the Philippines</h1>
-                    <p class="text-white mt-4 text-lg md:text-xl max-w-3xl mx-auto leading-relaxed">
-                        At Bestlink College of the Philippines, we provide and promote quality education with modern and unique techniques 
-                        to enhance the skills and knowledge of our dear students, making them globally competitive and productive citizens.
-                    </p>
-                    <button class="mt-8 px-6 py-3 bg-blue-600 bg-opacity-10 text-white font-semibold rounded-full border-4 border-blue-300 hover:bg-blue-300 hover:bg-opacity-100 transition duration-150">
-                        Enroll Now!
-                    </button>
-                </div>
-            </div>
-        </div>
+
