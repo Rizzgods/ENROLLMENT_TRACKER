2833727db989acecc57d1fe0894e0aef551b4fe3
yuwan <jcandaya0616@gmail.com>
2025-03-06 01:56:58 +0800

    ok na talaga

--- superadmin/list.php
+++ superadmin/list.php
@@ -5,25 +5,25 @@
         <!-- Sidebar (Counts) -->
         <div class="w-1/5 bg-white p-4 rounded-lg shadow-md flex flex-col">
             <h2 class="text-lg font-bold mb-4">Dashboard</h2>
-            <div class="bg-blue-500 text-white p-4 rounded-lg shadow-md text-center cursor-pointer border border-transparent hover:ring-4 hover:ring-gray-300 focus-within:ring-4 focus-within:ring-gray-300"
+            <div class="bg-blue-500 text-white p-4 rounded-lg shadow-md text-center border border-transparent hover:ring-4 hover:ring-gray-300 focus-within:ring-4 focus-within:ring-gray-300"
             data-target="AdminTable">
                 <h2 class="text-lg font-bold">Admins</h2>
                 <p class="text-2xl font-semibold"><?php echo $admin_count; ?></p> 
             </div>
             <br>
-            <div class="bg-green-500 text-white p-4 rounded-lg shadow-md text-center cursor-pointer" 
+            <div class="bg-green-500 text-white p-4 rounded-lg shadow-md text-center" 
             data-target="preEnrolleesTable">
                 <h2 class="text-lg font-bold">Pre-Enrollees</h2>
                 <p class="text-2xl font-semibold"><?php echo $pre_enrollees_count; ?></p> 
             </div>
             <br>
-            <div class="bg-yellow-500 text-white p-4 rounded-lg shadow-md text-center cursor-pointer" 
+            <div class="bg-yellow-500 text-white p-4 rounded-lg shadow-md text-center" 
             data-target="EnrolleesTable">
                 <h2 class="text-lg font-bold">Enrollees</h2>
                 <p class="text-2xl font-semibold"><?php echo $enrollees_count; ?></p> 
             </div>
             <br>
-            <div class="bg-red-500 text-white p-4 rounded-lg shadow-md text-center cursor-pointer" 
+            <div class="bg-red-500 text-white p-4 rounded-lg shadow-md text-center" 
             data-target="StudentTable">
                 <h2 class="text-lg font-bold">Students</h2>
                 <p class="text-2xl font-semibold"><?php echo $students_count; ?></p> 
