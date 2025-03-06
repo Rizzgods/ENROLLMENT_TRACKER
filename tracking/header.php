e9223467badfbbac86a29fbf73e0caac6463bbcd
Rizzgods <markryanceniza2002@gmail.com>
2025-03-07 01:22:09 +0800

    improve email + reset link 3

--- tracking/header.php
+++ tracking/header.php
@@ -6,7 +6,7 @@
         <div class="flex flex-col sm:flex-row justify-between items-center gap-4 sm:gap-0">
             <div class="flex items-center space-x-4">
                 <a href="profile.php" class="text-white hover:text-blue-200 transition duration-150">
-                    <img src="assets/logo" alt="Logo" class="h-10 w-10">
+                    <img src="https://admission.bcpsms4.com/pre_enroll/assets/logo.png" alt="Logo" class="h-10 w-10">
                 </a>
                 <h1 class="text-2xl font-bold tracking-tight"></h1>
             </div>
