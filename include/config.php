285fab477971b7d29c2f28e6a4f4bd28f1ad7789
Rizzgods <markryanceniza2002@gmail.com>
2025-03-06 21:08:32 +0800

    superadmin 5

--- include/config.php
+++ include/config.php
@@ -1,18 +1,23 @@
 <?php
+// Update database credentials for production server
 defined('server') ? null : define("server", "localhost");
-defined('user') ? null : define ("user", "root") ;
-defined('pass') ? null : define("pass","");
-defined('database_name') ? null : define("database_name", "dbgreenvalley") ;
+defined('user') ? null : define("user", "admi_greenvalley");
+defined('pass') ? null : define("pass", "xr9%kxu%*my^+kf2");
+defined('database_name') ? null : define("database_name", "admi_dbgreenvalley");
 
-$this_file = str_replace('\\', '/', __File__) ;
+// Path configurations
+$this_file = str_replace('\\', '/', __File__);
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
 
-$web_root =  str_replace (array($doc_root, "include/config.php") , '' , $this_file);
-$server_root = str_replace ('config/config.php' ,'', $this_file);
+$web_root = str_replace(array($doc_root, "include/config.php"), '', $this_file);
+$server_root = str_replace('config/config.php', '', $this_file);
 
+define('web_root', $web_root);
+define('server_root', $server_root);
 
-define ('web_root' , $web_root);
-define('server_root' , $server_root);
+// Add helpful constants for your application
+define('SITE_TITLE', 'Online Enrollment System');
 
-
+// Error reporting - change to 0 for production
+define('SHOW_ERROR_DETAIL', 0);
 ?>
