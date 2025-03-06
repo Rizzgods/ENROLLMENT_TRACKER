4d1b684b64ed3debe7f90f91df9deba369e4e6e4
Rizzgods <markryanceniza2002@gmail.com>
2025-03-06 20:10:35 +0800

    db5

--- pre_enroll/Logic_login.php
+++ pre_enroll/Logic_login.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dbgreenvalley";
+$username = "admi_greenvalley";
+$password = "xr9%kxu%*my^+kf2";
+$dbname = "admi_dbgreenvalley";
 
 // Create connection
 $conn = new mysqli(hostname: $servername, username: $username, password: $password, database: $dbname);
--- pre_enroll/cards.php
+++ pre_enroll/cards.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dbgreenvalley";
+$username = "admi_greenvalley";
+$password = "xr9%kxu%*my^+kf2";
+$dbname = "admi_dbgreenvalley";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
